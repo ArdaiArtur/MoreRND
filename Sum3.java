6668ca98c2383e3926a80ee3cd34e410400ed2d6
Artur Ardai <krkerdo@gmail.com>
2022-07-24 13:59:07 -1200

    bigfail

--- Sum3.java
+++ Sum3.java
@@ -1,15 +1,18 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class Sum3 {
     public static void main(String[] args) {
         int []nums={-1,0,1,2,-1,-4};
         threeSum(nums);
     }
-    public static List<List<Integer>> threeSum(int[] nums) {
+    public static List<List<Integer>> threeSumfail(int[] nums) {
         List<List<Integer>>fin=new LinkedList<>();
         HashMap<Integer,Boolean> man=new HashMap<>();
         for (int list : nums) {
@@ -48,5 +51,28 @@ public class Sum3 {
                 
         }
         return fin;
+    }
+
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> s = new HashSet<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            int start = i + 1;
+            int end = nums.length - 1;
+
+            while (start < end) {
+                if (nums[i] + nums[start] + nums[end] == 0) {
+                    s.add(Arrays.asList(nums[i], nums[start], nums[end]));
+                    start++;
+                    end--;
+                } else if (nums[i] + nums[start] + nums[end] > 0) {
+                    end--;
+                } else {
+                    start++;
+                }
+            }
+        }
+        return new ArrayList<>(s);
     }
 }
