14bcf7880d0bba630edfb6c6c0e9252bbb3c5899
Artur Ardai <krkerdo@gmail.com>
2022-08-01 20:11:29 +0300

    ye

---
+++ UglyNumber.java
@@ -0,0 +1,20 @@
+/**
+ * UglyNumber
+ */
+public class UglyNumber {
+
+    public static void main(String[] args) {
+        
+    }
+    public boolean isUgly(int n) {
+        if(n==0) return false;
+        if(n==1) return true;
+        while (n%2==0 ||n%3==0||n%5==0) {
+            if(n%2==0) n=n/2;
+            if(n%3==0) n=n/3;
+            if(n%5==0) n=n/5;
+        }
+        if(n==1) return true;
+        return false;
+    }
+}
