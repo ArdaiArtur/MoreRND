177a432d35e4fb92738403e24190a07731c6939b
Artur Ardai <krkerdo@gmail.com>
2022-08-02 21:35:44 +0300

    nope

---
+++ KthSmallestElementInASortedMatrix.java
@@ -0,0 +1,57 @@
+public class KthSmallestElementInASortedMatrix {
+    public static void main(String[] args) {
+        
+    }
+    public int kthSmallest(int[][] matrix, int k) {
+        if(matrix.length==1)
+        return matrix[0][0];
+        int j=k%matrix[0].length;
+        int set=0;
+        int fin=0;
+        for (int i = 0; i < matrix.length; i++) {
+            set+=matrix[0].length;
+            if(set>=k)
+            {   if(j==0)
+                fin=matrix[i][matrix[0].length-1];
+                else
+                fin=matrix[i][j-1];
+                break;
+            }
+        }
+        
+        int n = matrix.length;
+        int low = matrix[0][0];
+        int high = matrix[n-1][n-1];
+ 
+        while(low<high){
+ 
+            int mid = low + (high-low)/2;
+            int count = lessEqual(matrix , mid);
+ 
+            if(count<k) low = mid+1;
+            else high = mid;
+        }
+ 
+        return low;
+     }
+     
+      public int lessEqual(int[][] matrix , int t ){
+ 
+         int count=0;
+         int i= matrix[0].length-1;
+         int j=0;
+ 
+         while(i>=0 && j<matrix.length){
+ 
+             if(matrix[i][j]>t){
+                 i--;
+             }
+             else{
+                 count+=i+1;
+                 j++;
+             }
+         }
+ 
+         return count;
+    }
+}
