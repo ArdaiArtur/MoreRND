64638d13498dfa44715418911434832f0987d6d2
Artur Ardai <krkerdo@gmail.com>
2022-08-02 21:07:46 +0300

    not yet

--- SuperUglyNumber.java
+++ SuperUglyNumber.java
@@ -1,27 +1,43 @@
 public class SuperUglyNumber {
     public static void main(String[] args) {
-        nthSuperUglyNumber(12,new int[] {2,7,13,19});
+        nthSuperUglyNumber(29,new int[] {3,5,7,17,19,23,29,43,47,53});
     }
     public static int nthSuperUglyNumber(int n, int[] primes) {
         int[] dp = new int[n];
         int []man= new int [primes[primes.length-1]+1];
         dp[0] = 1;
-     
+     int db=0;
         for (int i = 1; i < n; i++) {
        int minx=1000;
        int sp=0;
+       
             for (int ix : primes) {
                 if(dp[man[ix]]*ix<minx)
-                sp=ix;
+                {
+                    sp=ix;
+                   
+                }
                 minx=Math.min(dp[man[ix]]*ix,minx);
 
             }
             dp[i] = minx;
-            man[sp]++;
+           int fd=minx;
+                while(fd%sp==0)
+                {
+                    fd=fd/sp;
+                } 
+               
+            if(man[fd]!=0&&db>man[fd])
+            { 
+                
+                man[fd]++; 
+                System.out.print(fd+" ");
+            }
+            man[sp]++; System.out.print(sp+" ");
+            System.out.println(dp[i]+" ");
+            db++;
         }
-        for (int i : dp) {
-            System.out.print(i+" ");
-        }
+        
         return dp[n - 1];       
     }
     }
