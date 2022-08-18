c95bafc6c67259ce7adb90d4f904e719edc6e400
Artur Ardai <krkerdo@gmail.com>
2022-08-18 13:38:55 +0300

    better

--- ReduceArraySizeToTheHalf.java
+++ ReduceArraySizeToTheHalf.java
@@ -1,9 +1,13 @@
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.PriorityQueue;
 
 public class ReduceArraySizeToTheHalf {
     public int minSetSize(int[] arr) {
         int length=arr.length;
-        int []count=new int [9999];
+        int []count=new int [100001];
         for (int i : arr) {
             count[i]++;
         }
@@ -20,5 +24,26 @@ public class ReduceArraySizeToTheHalf {
            
         }
         return fin;
+    }
+
+    public int minSetSizeefi(int[] arr) {
+    HashMap<Integer,Integer>hm=new HashMap<>();
+   int n=arr.length;
+    for (int i =0;i<n;i++) {
+       hm.put(arr[i], hm.getOrDefault(arr[i], 0)+1);
+    }
+   
+   PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
+     for (var i : hm.entrySet()) {
+        pq.add(i.getValue());
+    }
+    int res=0;
+    int count=0;
+    while(res<n/2 && pq.size()>0)
+    {
+        count++;
+        res+=pq.remove();
+    }
+    return count;
     }
 }
