5a03c571203cfda6f5ec1ee15b82d3f497ffbe6a
Artur Ardai <krkerdo@gmail.com>
2022-08-23 13:03:34 +0300

    ye

--- PalidromeLinkedList.java
+++ PalidromeLinkedList.java
@@ -1,5 +1,35 @@
 public class PalidromeLinkedList {
     public boolean isPalindrome(ListNode head) {
-        
+       
+         ListNode curr=head;
+         ListNode prev=null;
+         ListNode next=null;
+        var man=head;
+        int n=0; 
+        if(head == null || head.next == null){
+            return true;
+        }
+        while(man!=null&&man.next!=null)
+        {
+            n++;
+            man=man.next;
+        }
+       int mid=n/2;
+       while (mid!=0) {
+        next=curr.next;
+        curr.next=prev;
+        prev=curr;
+        curr=next;
+       }
+       if(n%2!=0)
+       curr=curr.next;
+       while (curr!=null) {
+        if(prev.val!=curr.val)
+        return false;
+        curr=curr.next;
+        prev=prev.next;
+       }
+       return true;
+       
     }
 }
