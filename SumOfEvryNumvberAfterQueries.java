5fb955d28b74c6632d9cc55820a442f1552cfc55
Artur Ardai <krkerdo@gmail.com>
2022-09-21 17:11:55 +0300

    done

--- SumOfEvryNumvberAfterQueries.java
+++ SumOfEvryNumvberAfterQueries.java
@@ -33,4 +33,29 @@ public class SumOfEvryNumvberAfterQueries {
         
         return fin;
     }
+
+    public int[] gsumEvenAfterQueries(int[] nums, int[][] queries) {
+        int sum=0;
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i]%2==0) sum+=nums[i];
+        }   
+         int [] fin =new int [nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            int a=queries[i][0];
+            int b=queries[i][1];
+            int cur=nums[b]+a;
+            if(nums[b]%2==0) sum-=nums[b];
+
+            nums[b]=cur;
+
+            if(cur%2==0) sum+=cur;
+            fin[i]=sum;
+        }
+        return fin;
+        
+    }
+
+
+
+
 }
