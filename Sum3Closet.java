18bc9f3cf823ecb7d8237a156cfbba373a4a064e
Artur Ardai <krkerdo@gmail.com>
2022-10-08 11:55:46 +0300

    yes ish

---
+++ Sum3Closet.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Sum3Closet {
+    public int threeSumClosest(int[] nums, int target) {
+        int count=Integer.MAX_VALUE;
+
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            int start = i + 1;
+            int end = nums.length - 1;
+
+            while (start < end) {
+                if (nums[i] + nums[start] + nums[end] == target) {
+                    return target;
+
+                   
+                } else if (nums[i] + nums[start] + nums[end] > target) {
+                    end--;
+                } else {
+                    start++;
+                }
+
+              
+            }
+          
+            if(Math.abs(count-target) >Math.abs((nums[i] + nums[start] + nums[end])-target))
+               if(start+1<nums.length)
+               {   count=nums[i] + nums[start] + nums[start+1];
+                System.out.println(nums[i]+" " + nums[start]+" " + nums[end]);
+               }
+            else
+                 count=nums[i] + nums[start] + nums[start-1];
+        }
+        return count;
+    }
+}
