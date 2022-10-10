835031d497cd540bb8e8c481bb92c48e22992827
Artur Ardai <krkerdo@gmail.com>
2022-10-10 19:55:55 +0300

    yes

--- TwoSumIVInputISaBST.java
+++ TwoSumIVInputISaBST.java
@@ -1,5 +1,17 @@
+import java.lang.annotation.Retention;
+import java.util.HashSet;
+import java.util.Set;
+
 public class TwoSumIVInputISaBST {
+    Set<Integer> res = new HashSet<>();
     public boolean findTarget(TreeNode root, int k) {
-        
+        if(root==null)
+        return false;
+    if(res.contains(k-root.val))
+        return true;
+    res.add(root.val);
+    return findTarget(root.left,k) || findTarget(root.right,k);
     }
+
+   
 }
