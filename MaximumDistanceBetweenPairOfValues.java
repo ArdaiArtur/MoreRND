7ac6085d23ac577f48e4024b9d5207cd1224c604
Artur Ardai <krkerdo@gmail.com>
2022-10-21 18:13:16 +0300

    agh

--- MaximumDistanceBetweenPairOfValues.java
+++ MaximumDistanceBetweenPairOfValues.java
@@ -1,22 +1,27 @@
-import java.lang.annotation.Retention;
+
 
 public class MaximumDistanceBetweenPairOfValues {
     public int maxDistance(int[] nums1, int[] nums2) {
-        int i=0,j=0,max=0;
-        while (i<nums1.length&&j<nums2.length) {
-            if(nums1[i]<=nums2[j])
-            {
-                j++;
+        int max = 0;
+        for (int i = 0; i < nums1.length; i++) {
+            int r = nums2.length - 1;
+            int l = i;
+            int m = i;
+            while (l <= r) {
+                m = l + (r - l) / 2;
+                if (nums1[i] > nums2[m]) {
+                    r = m - 1;
+                } else if (nums1[i] == nums2[m]) {
+                    l = m + 1;
+                } else {
+                    l = m + 1;
+                }
             }
-            else
-            {
-                i++;
-                j=i;
+            if (r < 0) {
+                continue;
             }
-           max= Math.max(max, j-i);
+            max = Math.max(max, r - i);
         }
-        
-    return max==0?max:max-1;
+        return max;
     }
-    
 }
