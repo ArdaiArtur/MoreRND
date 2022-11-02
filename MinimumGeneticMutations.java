d18f24a977439862f341dd2cf2b044305e6a74dc
Artur Ardai <krkerdo@gmail.com>
2022-11-02 20:51:44 +0300

    no but i learned

--- MinimumGeneticMutations.java
+++ MinimumGeneticMutations.java
@@ -1,4 +1,8 @@
-import java.lang.Thread.State;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class MinimumGeneticMutations {
@@ -41,4 +45,41 @@ public class MinimumGeneticMutations {
         System.out.println(start+" "+coco+" "+end);
         return start.equals(end)? count:-1;
     }
+
+    public int minMutationGood(String start, String end, String[] bank) {
+        Queue<String>qu=new LinkedList<>();
+        HashSet<String>hs=new HashSet<>();
+        qu.add(start);
+        hs.add(start);
+        int steps=0;
+        while(!qu.isEmpty())
+        {
+            int nodes=qu.size();
+            for (int i = 0; i < nodes; i++) {
+                String node=qu.poll();
+                if(node.equals(end))
+                {
+                    return steps;
+                }
+
+                for (char c: new char[] {'A', 'C', 'G', 'T'}) {
+                    for (int j = 0; j < node.length(); j++) {
+                        String neighbor = node.substring(0, j) + c + node.substring(j + 1);
+                        if (!hs.contains(neighbor) && Arrays.asList(bank).contains(neighbor)) {
+                            qu.add(neighbor);
+                            hs.add(neighbor);
+                        }
+                    }
+                }
+
+
+
+            }
+
+        }
+        return -1;
+    
+    }
+
+
 }
