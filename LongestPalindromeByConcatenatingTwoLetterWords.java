65d7b68658bf23689e3998a59edf7d29eb5be654
Artur Ardai <krkerdo@gmail.com>
2022-11-03 15:46:37 +0300

    I got so close but yet so far i didnt do  it in 45 min i failed

--- LongestPalindromeByConcatenatingTwoLetterWords.java
+++ LongestPalindromeByConcatenatingTwoLetterWords.java
@@ -34,4 +34,38 @@ public class LongestPalindromeByConcatenatingTwoLetterWords {
         }
         return ok?count*2+2:count*2;
     }
+
+    public int longestPalindromes(String[] words) {
+        int count=0;
+          HashMap<String,Integer>hm=new HashMap<>();
+          for (int i = 0; i < words.length; i++) {
+                  hm.put(words[i], hm.getOrDefault(words[i], 0)+1);
+          }
+        boolean ok=false;
+          for (String word : hm.keySet()) {
+          
+      
+              
+              if(word.charAt(0)==word.charAt(1))
+              {
+                  if(hm.get(word)%2==0)
+                  count+=hm.get(word);
+                  else
+                  {
+                   count+=hm.get(word)-1;
+                       ok=true;
+                  }
+              }
+              else if (word.charAt(0) < word.charAt(1)) 
+              { 
+                  String s=word.charAt(1)+""+word.charAt(0);
+                  if(hm.containsKey(s))
+                  {
+                      count+=2*Math.min(hm.get(word), hm.get(s));
+                  
+                  }
+              }
+          }
+          return ok?count*2+2:count*2;
+      }
 }
